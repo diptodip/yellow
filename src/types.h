3d2910385decdc0ab06242018593cac0c0390fd3
Diptodip Deb <d2d.tar.gz@gmail.com>
2020-06-09 22:13:24 -0400

    Add type aliases

---
+++ src/types.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+typedef double f64;
+typedef float f32;
+typedef int64_t i64;
+typedef uint64_t u64;
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef int16_t i16;
+typedef uint16_t u16;

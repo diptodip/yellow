3dc4f25a2bf3d0b0489837f71cf154f2bd1fa76b
Diptodip Deb <dd@d2d.sh>
2021-03-14 03:11:40 -0400

    Add skeleton for intersection tracking

--- src/ray.h
+++ src/ray.h
@@ -3,12 +3,23 @@ struct Ray {
 	Vec3D direction;
 };
 
-struct Intersection {
-	Point3D origin;
-	Vec3D normal;
+Ray bounce(Ray *ray, Point3D off) {
+	return (Ray) {off, ray->direction};
+}
+
+Ray reflect(Ray *ray, Vec3D normal) {
+	Vec3D direction = ray->direction;
+	Vec3D reflected = direction - (2 * dot(&direction, &normal) * normal);
+	return (Ray) {ray->origin, reflected};
+}
+
+struct Intersections {
+	Point3D *origins;
+	Vec3D *normals;
 };
 
 // TODO(dd): implement struct of arrays to hold intersection information
 // TODO(dd): implement ray tracing algorithm
-void trace(Ray *r, Traceable *world, Intersection *intersections) {
+RGBA trace(Ray *ray, Traceable *world, Intersections *intersections) {
+	return (RGBA) {};
 }

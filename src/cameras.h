9b80e2ac6024a255e906b2bb573e093d98957ac7
Diptodip Deb <dd@d2d.sh>
2021-03-28 14:44:12 -0400

    Add prime ray function

--- src/cameras.h
+++ src/cameras.h
@@ -24,6 +24,25 @@ Ray prime_ray(Camera *camera, u32 row, u32 col) {
 	return Ray {};
 }
 
-u32* imalloc(size_t height, size_t width) {
-	return (u32*) malloc(sizeof(u32) * height * width);
+Ray prime_ray(Camera *camera, f64 row_frac, f64 col_frac) {
+	Vec3D basis1 = cross(&camera->up, &camera->normal);
+	basis1 = normalize(&basis1);
+	Vec3D basis2 = cross(&camera->normal, &basis1);
+	f64 lens_radius = camera->aperture / 2.0;
+	Vec3D random_lens_offset = lens_radius * random_unit_vector();
+	random_lens_offset = (basis1 * random_lens_offset.x) + (basis2 * random_lens_offset.y);
+	Point3D top_left = (camera->origin
+			    - (camera->focal_distance * camera->image_plane.width * basis1 / 2.0)
+			    + (camera->focal_distance * camera->image_plane.height * basis2 / 2.0)
+			    - (camera->focal_distance * camera->normal));
+	Vec3D direction = (top_left
+			   + (camera->focal_distance * basis1 * col_frac * camera->image_plane.width)
+			   - (camera->focal_distance * basis2 * row_frac * camera->image_plane.height)
+			   - camera->origin
+			   - random_lens_offset);
+	return (Ray) {camera->origin + random_lens_offset, direction};
+}
+
+u32* imalloc(size_t rows, size_t cols) {
+	return (u32*) malloc(sizeof(u32) * rows * cols);
 }

e24fc864c079ce14e9344009b7815e81d13fce72
Diptodip Deb <dd@d2d.sh>
2021-06-11 23:23:10 -0400

    Remove prime ray creation from cameras.h

--- src/cameras.h
+++ src/cameras.h
@@ -30,25 +30,6 @@ ImagePlane create_image_plane(f64 fov, f64 aspect_ratio, u32 pixel_height) {
 	return image_plane;
 }
 
-Ray prime_ray(Camera *camera, f64 row_frac, f64 col_frac) {
-	Vec3D basis1 = cross(&camera->up, &camera->normal);
-	basis1 = normalize(&basis1);
-	Vec3D basis2 = cross(&camera->normal, &basis1);
-	f64 lens_radius = camera->aperture / 2.0;
-	Vec3D random_lens_offset = lens_radius * random_unit_vector();
-	random_lens_offset = (basis1 * random_lens_offset.x) + (basis2 * random_lens_offset.y);
-	Point3D top_left = (camera->origin
-			    - (camera->focal_distance * camera->image_plane.width * basis1 / 2.0)
-			    + (camera->focal_distance * camera->image_plane.height * basis2 / 2.0)
-			    - (camera->focal_distance * camera->normal));
-	Vec3D direction = (top_left
-			   + (camera->focal_distance * basis1 * col_frac * camera->image_plane.width)
-			   - (camera->focal_distance * basis2 * row_frac * camera->image_plane.height)
-			   - camera->origin
-			   - random_lens_offset);
-	return (Ray) {camera->origin + random_lens_offset, direction};
-}
-
 u32* imalloc(size_t rows, size_t cols) {
 	return (u32*) malloc(sizeof(u32) * rows * cols);
 }

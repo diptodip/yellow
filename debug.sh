4c58b7de341fc82e7c962d2ccf08204046ace9c3
Diptodip Deb <dd@d2d.sh>
2021-06-27 22:46:10 -0400

    Remove verbose build

--- debug.sh
+++ debug.sh
@@ -2,5 +2,5 @@
 
 rm -rf targets;
 mkdir targets;
-clang++ -Ofast -v -std=c++11 -lm -fopenmp -o targets/yellow src/yellow.cpp;
+clang++ -Ofast -ffast-math -std=c++14 -lm -pthread -o targets/yellow src/yellow.cpp;
 ./targets/yellow;

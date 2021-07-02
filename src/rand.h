c4a3cf377741eba9bdf77705f2372d9c0a280083
Diptodip Deb <dd@d2d.sh>
2021-07-02 19:50:00 -0400

    Store random state explicitly in struct

--- src/rand.h
+++ src/rand.h
@@ -1,24 +1,25 @@
 #ifndef YELLOW_RAND
 #define YELLOW_RAND
-#include <ctime>
 #include "types.h"
 
-static thread_local u32 global_rng_state = (u32) time(NULL);
+struct PRNGState {
+	u32 entropy;
+};
 
-static inline u32 xor_shift32() {
-	u32 x = global_rng_state;
+inline u32 xor_shift32(PRNGState *prng_state) {
+	u32 x = prng_state->entropy;
 	x ^= x << 13;
 	x ^= x >> 7;
 	x ^= x << 17;
-	global_rng_state = x;
+	prng_state->entropy = x;
 	return x;
 }
 
-f32 unit_uniform() {
-	return xor_shift32() / (f32) UINT32_MAX;
+inline f32 unit_uniform(PRNGState *prng_state) {
+	return xor_shift32(prng_state) / (f32) UINT32_MAX;
 }
 
-f32 uniform(f32 start, f32 end) {
-	return (unit_uniform() * (end - start)) + start;
+inline f32 uniform(PRNGState *prng_state, f32 start, f32 end) {
+	return (unit_uniform(prng_state) * (end - start)) + start;
 }
 #endif //YELLOW_RAND

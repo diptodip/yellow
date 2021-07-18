7c263685270e037885b518f24cd2ea308b59906a
Diptodip Deb <dd@d2d.sh>
2021-07-18 04:02:01 -0400

    Update build scripts to include randomness source libraries

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 @rmdir /s /q .\targets
 @mkdir .\targets
-clang.exe -Ofast -ffast-math -llibcmt -std=c++14 -o .\targets\yellow.exe .\src\yellow.cpp
+clang.exe -Ofast -ffast-math -llibcmt -lbcrypt -std=c++14 -o .\targets\yellow.exe .\src\yellow.cpp
 .\targets\yellow.exe
--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 rm -rf targets;
 mkdir targets;
-clang++ -Ofast -ffast-math -std=c++14 -lm -pthread -o targets/yellow src/yellow.cpp;
+clang++ -Ofast -ffast-math -std=c++14 -lm -lbsd -pthread -o targets/yellow src/yellow.cpp;
 ./targets/yellow;

b0d0f07aecf0aac45fa5631ec038a30dce94b02e
Diptodip Deb <dd@d2d.sh>
2021-07-19 03:10:12 -0400

    Update randomness requirements in build scripts

--- debug.bat
+++ debug.bat
@@ -1,5 +1,5 @@
 @echo off
 @rmdir /s /q .\targets
 @mkdir .\targets
-clang.exe -O0 -g -ffast-math -llibcmt -std=c++14 -o .\targets\yellow_debug.exe .\src\yellow.cpp
+clang.exe -O0 -g -ffast-math -llibcmt -lbcrypt -std=c++14 -o .\targets\yellow_debug.exe .\src\yellow.cpp
 .\targets\yellow_debug.exe

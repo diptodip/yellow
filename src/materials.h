3d0cb42af8e46095d95f8ad2a48a044decfb40cf
Diptodip Deb <dd@d2d.sh>
2021-07-24 22:54:36 -0400

    Add planes to the world

--- src/materials.h
+++ src/materials.h
@@ -18,10 +18,18 @@ struct Sphere {
 	u32 material_index;
 };
 
+struct Plane {
+	Vec3D normal;
+	f32 distance;
+	u32 material_index;
+};
+
 struct World {
 	u32 num_materials;
 	u32 num_spheres;
+	u32 num_planes;
 	Material *materials;
 	Sphere *spheres;
+	Plane *planes;
 };
 #endif //YELLOW_MATERIALS
